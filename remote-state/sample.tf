42693a0d4db014e460710122dff772de0dacfaac
kiran1616 <kirankumar7163@gmail.com>
2023-02-07 22:19:25 +0530

    updated script

---
+++ remote-state/sample.tf
@@ -0,0 +1,90 @@
+variable "sample" {
+  default = 10
+}
+
+output "sample" {
+  value = var.sample
+}
+
+#datatyppes
+#we have 3 types string data, booleandata, numberdata
+
+#string data type
+variable "sample1" {
+  default = "hi this is kiran"
+}
+
+#numberdatatype
+variable "sample2" {
+  default = 100
+}
+
+#booleandatatype
+variable "sample3" {
+  default = false
+}
+
+#list of variable type
+#1 default variable type
+#2 list variable type
+#3 mapping variable type
+
+variable "sample4" {
+  default = "hi kiran"
+}
+
+output "sample4" {
+  value = var.sample4
+}
+
+#list variable type
+variable "sample5" {
+  default = [
+    101,
+    "kiran",
+    "pravalika"
+  ]
+}
+
+output "sample5" {
+  value = var.sample5[2]
+}
+
+#mapping variable
+
+variable "sample6" {
+  default = {
+    number = 100
+    string = "kiran pravalika"
+    boolean = true
+  }
+}
+
+output "sample6" {
+  value = var.sample6["number"]
+
+}
+
+#terraform.tfvar
+
+variable "demo1" {}
+output "demo1" {
+  value = var.demo1
+}
+variable "demo2"{
+  default = "hi 123"
+}
+
+output "demo2" {
+  value = var.demo2
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "kiranprav"
+    key    = "remote-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
