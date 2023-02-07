ce59293af7d4a4d6e49fa8457de2f6f43e67d105
kiran1616 <kirankumar7163@gmail.com>
2023-02-07 19:06:54 +0530

    updated script

--- resource/sample.tf
+++ resource/sample.tf
@@ -6,6 +6,7 @@ data "aws_ami" "centos8" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
+
 }
 
 resource "aws_instance" "web" {
@@ -15,4 +16,11 @@ resource "aws_instance" "web" {
   tags = {
     Name = "testing"
   }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = 'aws_vpc.04018a37c2a1f4629.id'
+  cidr_block        = "172.31.0.0/16"
+  availability_zone = "us-east-1a"
+
 }
