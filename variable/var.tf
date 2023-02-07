82ef811af8a632750322f3e041b383b30e7aedab
kiran1616 <kirankumar7163@gmail.com>
2023-02-07 15:13:14 +0530

    updated script

--- variable/var.tf
+++ variable/var.tf
@@ -5,3 +5,66 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+#datatyppes
+#we have 3 types string data, booleandata, numberdata
+
+#string data type
+variable "sample1" {
+  default = "hi this is kiran"
+}
+
+#numberdatatype
+
+variable "sample2" {
+  default = 100
+}
+
+#booleandatatype
+
+variable "sample3" {
+  default = fales
+}
+
+#list of variable type
+#1 default variable type
+#2 list variable type
+#3 mapping variable type
+
+variable "sample4" {
+  default = "hi kiran"
+
+}
+
+output "sample4" {
+  value = var.sample4
+
+}
+
+#list variable type
+variable "sample5" {
+  default = [
+    101,
+    kiran,
+    pravalika
+  ]
+}
+
+output "sample5" {
+  value = var.sample5[2]
+}
+
+#mapping variable
+
+variable "sample6" {
+  default = {
+    number = 100
+    string = "kiran pravalika"
+    boolean = true
+  }
+}
+
+output "sample6" {
+  value = var.sample6["number"]
+
+}
