be1314b83e86e29a25be1e96c9fb425800196b21
kiran1616 <kirankumar7163@gmail.com>
2023-02-07 23:19:30 +0530

    updated script

--- resource/sample.tf
+++ resource/sample.tf
@@ -1,6 +1,6 @@
 
 
-
+#create a VPC
 resource "aws_vpc" "myvpc"{
 cidr_block = "10.0.0.0/16"
 tags = {
@@ -8,14 +8,14 @@ Name = "MyVPC"
 }
 }
 
-2: Create a public subnet
+#2: Create a public subnet
 resource "aws_subnet" "PublicSubnet"{
 vpc_id = aws_vpc.myvpc.id
 availability_zone = "us-east-1a"
 cidr_block = "10.0.1.0/24"
 }
 
-3 : create a private subnet
+#3 : create a private subnet
 resource "aws_subnet" "PrivSubnet"{
 vpc_id = aws_vpc.myvpc.id
 cidr_block = "10.0.2.0/24"
@@ -24,12 +24,12 @@ map_public_ip_on_launch = true
 }
 
 
-4 : create IGW
+#4 : create IGW
 resource "aws_internet_gateway" "myIgw"{
 vpc_id = aws_vpc.myvpc.id
 }
 
-5 : route Tables for public subnet
+#5 : route Tables for public subnet
 resource "aws_route_table" "PublicRT"{
 vpc_id = aws_vpc.myvpc.id
 route {
@@ -39,7 +39,7 @@ gateway_id = aws_internet_gateway.myIgw.id
 }
 
 
-7 : route table association public subnet
+#7 : route table association public subnet
 resource "aws_route_table_association" "PublicRTAssociation"{
 subnet_id = aws_subnet.PublicSubnet.id
 route_table_id = aws_route_table.PublicRT.id
