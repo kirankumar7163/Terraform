c7ba57cd261ac83d3275315d7839fede7ab49e01
kiran1616 <kirankumar7163@gmail.com>
2023-02-08 16:12:36 +0530

    updated script

--- ec2instance/ec2.tf
+++ ec2instance/ec2.tf
@@ -1,43 +1,7 @@
 
-
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "myvpcexample"
-  }
-}
-
-resource "aws_subnet" "mypublic_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "mypublic-example"
-  }
-}
-
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.mypublic_subnet.id
-  private_ips = ["10.0.1.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-resource "aws_instance" "foo" {
+resource "aws_instance" "example" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 1
-  }
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
+  subnet_id     = "172.31.0.0/24"
+  vpc_id        = "172.31.0.0/16"
 }
-
