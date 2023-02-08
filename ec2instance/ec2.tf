66204349038c9718caaf0843ca810e45de58ec42
kiran1616 <kirankumar7163@gmail.com>
2023-02-08 14:36:36 +0530

    updated script

--- ec2instance/ec2.tf
+++ ec2instance/ec2.tf
@@ -1,8 +1,39 @@
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-00149760ce42c967b"
-  instance_type = "t3.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "myvpcexample"
+  }
+}
+
+resource "aws_subnet" "mypublic_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "mypublic-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.mypublic_subnet.id
+  private_ips = ["10.0.0.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0d03b1ad793d7ac93 "
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
   }
 }
--- resource/sample.tf
+++ resource/sample.tf
@@ -9,24 +9,12 @@ data "aws_ami" "centos8" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
+
   tags = {
     Name = "testing"
   }
 }
 
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "testing"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
 
 #create a VPC
 resource "aws_vpc" "myvpc"{
