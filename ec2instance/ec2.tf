158329ce9a6670add4ec31633cafa080993e3078
kiran1616 <kirankumar7163@gmail.com>
2023-02-08 16:13:39 +0530

    updated script

--- ec2instance/ec2.tf
+++ ec2instance/ec2.tf
@@ -3,5 +3,4 @@ resource "aws_instance" "example" {
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
   subnet_id     = "172.31.0.0/24"
-  vpc_id        = "172.31.0.0/16"
-}
+  }
