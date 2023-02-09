03f43745e11345668e913c3772cf5f960a4b94d2
kiran1616 <kirankumar7163@gmail.com>
2023-02-09 17:14:10 +0530

    updated script

--- ec2instance/ec2.tf
+++ ec2instance/ec2.tf
@@ -1,6 +1,19 @@
 
-resource "aws_instance" "example" {
-  ami           = "ami-0a017d8ceb274537d"
-  instance_type = "t2.micro"
-  subnet_id     = "172.31.0.0/24"
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet"
   }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+}
