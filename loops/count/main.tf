a03c90571b6bc6ac3f14404c4955aba200dd1ec7
kiran1616 <kirankumar7163@gmail.com>
2023-02-09 17:17:48 +0530

    updated script

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
@@ -15,15 +20,6 @@ data "aws_ami" "centos8" {
 }
 
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_ami" "centos8" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
 
 #create a VPC
 resource "aws_vpc" "myvpc"{
